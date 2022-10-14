a8b4e25171220b74cc09d57d150a7531cb501d7f
fibr <fabry.d@seznam.cz>
2022-10-14 23:10:02 +0200

    uprava server.js dle zadani

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 const express = require('express')
 const config = require('./src/config/config')
 const cookieParser = require('cookie-parser')
-const mysql = require('mysql')
-
+const { sequelize } = require('./src/models')
 const app = express()
+
 app.use(
 	express.json({
 		type: ['application/json', 'text/plain'],
@@ -14,21 +14,7 @@ app.use(cookieParser())
 
 require('./src/routes')(app)
 
-const connection = mysql.createConnection({
-	host: config.db.options.host,
-	user: config.db.user,
-	password: config.db.password,
-	database: config.db.database,
+sequelize.sync().then(result => {
+	app.listen(config.app_port)
+	console.log(`Server is running at port ${config.app_port}`)
 })
-
-connection.connect(err => {
-	if (err) {
-		console.log('Error connection to MySQL database: ', err)
-		return
-	}
-	console.log('MySQL successfully connected!')
-})
-
-app.listen(config.app_port, () =>
-	console.log(`server running on PORT ${config.app_port}`)
-)
